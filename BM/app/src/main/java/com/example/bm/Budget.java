21d2174b7b4cc7b6470fb9b07f1267fc3aa4d7d4
ayaha99 <51969125+ayaha99@users.noreply.github.com>
2021-10-22 21:03:12 -0400

    Ayah's Unit Test
    Ayah's Unit Test

--- BM/app/src/main/java/com/example/bm/Budget.java
+++ BM/app/src/main/java/com/example/bm/Budget.java
@@ -1,8 +1,12 @@
 package com.example.bm;
 
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
+
 // Class for setting the budget
 public class Budget {
-
+    private static DecimalFormat df = new DecimalFormat("0.00");
     double budgetValue;
 
     public Budget(double budgetValue) {
@@ -17,6 +21,33 @@ public class Budget {
         this.budgetValue = budgetValue;
     }
 
+    // Function to convert to string
+    @Override
+    public String toString() {
+        return "$" + budgetValue;
+    }
+
+    // Function to check if budget is 0
+    public boolean checkIfBudgetZero() {
+        if(budgetValue == 0.00) {
+            return true;
+        }
+        else return false;
+    }
+
+    // Function to check if budget is less than 0
+    public boolean checkIfBudgetNegative() {
+        if(budgetValue < 0) {
+            return true;
+        }
+        else return false;
+    }
+
+    // Function to make sure 2 decimal places
+    public double convertToTwoDecimals() {
+        double budgetRounded = Math.round(budgetValue * 100.0)/100.0;
+        return budgetRounded;
+    }
 }
 
 
